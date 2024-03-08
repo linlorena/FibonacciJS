1a1e5056588e0175188f0520227b9753405f0521
linlorena <142946995+linlorena@users.noreply.github.com>
2024-03-08 03:55:01 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var n = prompt("Quantidade de termos da sequência: ");
+
+var termo1 = 0;
+var termo2 = 1;
+var termo3;
+
+document.write(termo1 + ", " + termo2);
+
+for (i=3; i<=n; i++){
+    termo3 = termo1 + termo2;
+    document.write(", " + termo3);
+    termo1 = termo2;
+    termo2 = termo3;
+}
